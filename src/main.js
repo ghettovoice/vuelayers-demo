000ef6c7be5a684afb647f72d912bb94614e7cdc
ghettovoice <ghettovoice@gmail.com>
2017-11-25 19:09:00 +0300

    initial commit: demo migration

---
+++ src/main.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Buefy from 'buefy'
+import 'buefy/lib/buefy.css'
+// import VueLayers modules
+import VueLayers from 'vuelayers'
+// import VueLayers styles
+import 'vuelayers/lib/style.css'
+import App from './App.vue'
+
+Vue.config.productionTip = false
+Vue.use(Buefy, {
+  defaultIconPack: 'fa',
+})
+// register all VueLayers components
+Vue.use(VueLayers)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  render: h => h(App),
+})
